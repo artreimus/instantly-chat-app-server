228c9c34c265b838116aaf69cf5c3aefca5bb272
artreimus <arthurreimus1623@gmail.com>
2023-08-05 02:07:57 +0800

    add subscription, user, and session types

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,28 +1,37 @@
-import { ISODateString, Session } from 'next-auth';
+import { ISODateString } from 'next-auth';
 import { Prisma, PrismaClient } from '@prisma/client';
 import {
   conversationPopulated,
   participantPopulated,
 } from '../graphql/resolvers/conversation';
+import { Context } from 'graphql-ws/lib/server';
+import { PubSub } from 'graphql-subscriptions';
 
+// Server configuration
 export interface GraphQLContext {
   session: Session | null;
   prisma: PrismaClient;
-  // pubsub
+  pubsub: PubSub;
+}
+
+export interface Session {
+  user?: User;
+  expires: ISODateString;
+}
+
+export interface SubscriptionContext extends Context {
+  connectionParams: {
+    session?: Session;
+  };
 }
 
 // User types:
 
-// export interface Session {
-//   user?: User;
-//   expires: ISODateString;
-// }
-
-// export interface User {
-//   id: string;
-//   username: string;
-//   image: string;
-// }
+export interface User {
+  id: string;
+  username: string;
+  image: string;
+}
 
 export interface CreateUsernameResponse {
   success?: boolean;
